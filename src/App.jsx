66b00cfa73063e8d9373388eefbf9929d1b17ddd
Vidit-01 <viditanupgupta@gmail.com>
2025-06-23 19:07:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div class="curve-container">
+        <svg viewBox="0 0 1440 100" class="curve">
+          <path
+            fill="#4CAF50"
+            d="M0,0 C480,100 960,0 1440,100 L1440,0 L0,0 Z"
+          ></path>
+        </svg>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <div className="logo">
+            
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
